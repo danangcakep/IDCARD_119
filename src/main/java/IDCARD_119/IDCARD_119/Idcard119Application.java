8389d2de9895154b69dcbdc757e3f3124cb3736c
Microsoft <Microsoft@DESKTOP-OSQH96F>
2022-11-14 21:36:16 +0700

    CREATE PROJECT

---
+++ src/main/java/IDCARD_119/IDCARD_119/Idcard119Application.java
@@ -0,0 +1,13 @@
+package IDCARD_119.IDCARD_119;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idcard119Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Idcard119Application.class, args);
+	}
+
+}
---
+++ src/test/java/IDCARD_119/IDCARD_119/Idcard119ApplicationTests.java
@@ -0,0 +1,13 @@
+package IDCARD_119.IDCARD_119;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Idcard119ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
